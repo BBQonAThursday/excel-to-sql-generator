03f4459920c670a8583f653a59bf5c6148229aba
Joel Gamble <joel.gamble@springswindowfashions.com>
2021-10-26 15:44:26 -0500

    adding in container-fluid search

--- index.js
+++ index.js
@@ -9,15 +9,29 @@ const url = 'https://www.baliblinds.com/motorization'
 axios(url).then(response => {
   const html = response.data;
   const $ = cheerio.load(html);
+  const containerSearches = ['.container', '.container-fluid'];
   const containers = [];
   $('.container', html).each(function() {
     const container = $(this).attr('class');
+    const firstChild = $(this).children().attr('class');
     // const url = $(this).find('a').attr('href');
     containers.push({
       container,
+      firstChild,
       url
     });
   });
+
+  $('.container-fluid', html).each(function() {
+    const containerFluid = $(this).attr('class');
+    const firstChild = $(this).children().attr('class');
+    // const url = $(this).find('a').attr('href');
+    containers.push({
+      containerFluid,
+      firstChild,
+      url
+    });
+  })
   console.log(containers)
 }).catch(err => console.log(err));
 
